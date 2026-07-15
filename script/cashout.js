b653948716da9617dbd729e02ee6914650f4d1ad
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-07-15 12:45:35 +0600

    24-8 Design Cashout Form and logical implementation

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-btn").addEventListener("click", function() {
+    // 1 - get the agent number & validate 
+    // 2 - get the amount, validate, convert to number 
+    // 3 - get the current balance, validate, convert to number 
+    // 4 - Calculate new balance 
+    // 5 - Get the pin and verify 
+    // 5.1 - true:: show an alert > set Balance 
+    // 5.2 - true:: show anerror alert > return 
+})
